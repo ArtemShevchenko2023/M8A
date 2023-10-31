5e0b4f395eaaf2576d4ef64b58fcd4c1329fa2c0
ArtemShevchenko2023 <129595378+ArtemShevchenko2023@users.noreply.github.com>
2023-10-31 23:59:14 +0400

    Update main_logic.py

--- main_logic.py
+++ main_logic.py
@@ -1,7 +1,10 @@
+#Логика проекта MCBot
+#Импорт библиотек
 import time
 from keras.models import load_model
 from PIL import Image, ImageOps
 import numpy as np
+#Функция определения моба из майнкрафта
 def get_class(path):
     np.set_printoptions(suppress=True)
     model = load_model("keras_model.h5", compile=False)
@@ -20,11 +23,13 @@ def get_class(path):
     print("Class:", class_name[2:-1], end="")
     print("Confidence Score:", confidence_score)
     return class_name[2:-1]
+#Время Самары
 def localtime():
     seconds = time.time() + 202
     local_time = time.ctime(seconds)
     return local_time
 localtime()
+#Время Москвы
 def loctime():
     seconds = time.time() + 202 - 3600
     locatime = time.ctime(seconds)
